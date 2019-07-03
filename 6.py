74d6f7e835ce1a37ee0643ee44c812f3225e5eb9
shenbanakshetha24 <51950533+shenbanakshetha24@users.noreply.github.com>
2019-07-03 15:29:37 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,6 +1,6 @@
 fr=input()
-for i in range(0,len(del)):
- if(de[i].isalpha() and de[i].isdigit()):
+for i in range(0,len(fr)):
+ if(fr[i].isalpha() and fr[i].isdigit()):
   print("No")
  else:
   print("Yes")
